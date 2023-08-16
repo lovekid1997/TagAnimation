68c72338804632b13ae88174d4aa5ff02d301dd4
vinhNguyen <beminhquan3@gmail.com>
2023-08-16 23:05:21 +0700

    init

---
+++ example/lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:tag_animation/tag_animation_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var tagAnimationController = TagAnimationController();
+  @override
+  void initState() {
+    tagAnimationController.onAddAll(
+      builder: (index) {
+        var tagName = 'tagName$index';
+        return TagWrapper(
+          tagName: tagName,
+          rawChip: RawChip(
+            label: Text(tagName),
+            onPressed: () {
+              var tagName2 = '$tagName${UniqueKey()}';
+              tagAnimationController.onAdd(
+                TagWrapper(
+                  tagName: tagName2,
+                  rawChip: RawChip(
+                    label: Text(tagName2),
+                    onDeleted: () {
+                      tagAnimationController.onDelete(tagName2);
+                    },
+                  ),
+                ),
+              );
+            },
+            onDeleted: () {
+              tagAnimationController.onDelete(tagName);
+            },
+          ),
+        );
+      },
+      itemCount: 1,
+    );
+    super.initState();
+  }
+
+  TagWrapper _test(String tagName) {
+    return TagWrapper(
+      tagName: tagName,
+      rawChip: RawChip(
+        label: Text(tagName),
+        onPressed: () {
+          var tagName2 = '$tagName${UniqueKey()}';
+          tagAnimationController.onAdd(
+            TagWrapper(
+              tagName: tagName2,
+              rawChip: RawChip(
+                label: Text(tagName2),
+                onDeleted: () {
+                  tagAnimationController.onDelete(tagName2);
+                },
+              ),
+            ),
+          );
+        },
+        onDeleted: () {
+          tagAnimationController.onDelete(tagName);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          TagAnimation(
+            controller: tagAnimationController,
+            height: 50.0,
+            animationType: TagAnimationType.transition,
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  tagAnimationController.onAdd(_test('on Add'));
+                },
+                child: const Text('onAdd'),
+              ),
+              TextButton(
+                onPressed: () {
+                  tagAnimationController.onInsert(_test('on Insert'), 0);
+                },
+                child: const Text('onInsert'),
+              ),
+              TextButton(
+                onPressed: () {
+                  tagAnimationController.onDeleteMulti(['on Add', 'on Insert']);
+                },
+                child: const Text('onDeleteMulti'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/tag_animation_widget.dart
@@ -0,0 +1,211 @@
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+
+enum TagAnimationType {
+  transition,
+}
+
+class TagAnimation extends StatefulWidget {
+  const TagAnimation({
+    super.key,
+    required this.controller,
+    required this.height,
+    this.animationType = TagAnimationType.transition,
+    this.scrollDirection = Axis.vertical,
+  });
+
+  final TagAnimationController controller;
+  final double height;
+  final TagAnimationType animationType;
+  final Axis scrollDirection;
+  @override
+  State<TagAnimation> createState() => _TagAnimationState();
+}
+
+class _TagAnimationState extends State<TagAnimation> {
+  final List<TagWrapper> _list = [];
+  StreamSubscription? _subscription;
+  @override
+  void initState() {
+    _subscription =
+        widget.controller._streamController.stream.listen(onListener);
+
+    super.initState();
+  }
+
+  void onListener(Map<String, dynamic> data) {
+    switch (data.keys.firstOrNull) {
+      case 'onDelete':
+        _onDelete(data);
+        break;
+      case 'onAdd':
+        _onAdd(data);
+        break;
+      case 'onAddAll':
+        _onAddAll(data);
+        break;
+      case 'onInsert':
+        _onInsert(data);
+        break;
+      case 'onDeleteMulti':
+        _onDeleteMulti(data);
+        break;
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.controller._streamController.close();
+    _subscription?.cancel();
+    _subscription = null;
+    super.dispose();
+  }
+
+  var animateKey = GlobalKey<AnimatedListState>();
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: widget.height,
+      child: AnimatedList(
+        scrollDirection: widget.scrollDirection,
+        key: animateKey,
+        itemBuilder: (context, index, animation) => _AnimationWidget(
+          animation: animation,
+          type: widget.animationType,
+          child: _list[index].rawChip,
+        ),
+        initialItemCount: _list.length,
+      ),
+    );
+  }
+
+  void _onDelete(Map<String, dynamic> data) {
+    final tagName = data['onDelete'] as String;
+    _deleteItemByTagName(tagName);
+  }
+
+  void _deleteItemByTagName(String tagName) {
+    var currentIndex = -1;
+    final tagWrapper = _list.firstWhereIndexedOrNull((index, e) {
+      currentIndex = index;
+      return e.tagName == tagName;
+    });
+    if (tagWrapper == null) {
+      return;
+    }
+    _list.removeAt(currentIndex);
+    animateKey.currentState!.removeItem(
+      currentIndex,
+      (context, animation) => _AnimationWidget(
+        animation: animation,
+        type: widget.animationType,
+        child: tagWrapper.rawChip,
+      ),
+    );
+  }
+
+  void _onAdd(Map<String, dynamic> data) {
+    _list.add(data['onAdd']);
+    animateKey.currentState!.insertItem(
+      _list.length - 1,
+    );
+  }
+
+  void _onAddAll(Map<String, dynamic> data) {
+    _list.addAll(data['onAddAll']);
+    animateKey.currentState!.insertAllItems(0, _list.length);
+  }
+
+  void _onInsert(Map<String, dynamic> data) {
+    final index = data['index'];
+    final tagWrapper = data['onInsert'] as TagWrapper;
+    _list.insert(index, tagWrapper);
+    animateKey.currentState?.insertItem(index);
+  }
+
+  void _onDeleteMulti(Map<String, dynamic> data) {
+    final tagNames = data['onDeleteMulti'] as List<String>;
+    for (var tagName in tagNames) {
+      _deleteItemByTagName(tagName);
+    }
+  }
+}
+
+class TagAnimationController {
+  final StreamController<Map<String, dynamic>> _streamController =
+      StreamController<Map<String, dynamic>>();
+  void onDelete(String tagName) {
+    _streamController.sink.add({'onDelete': tagName});
+  }
+
+  void onAdd(TagWrapper tagWrapper) {
+    _streamController.sink.add({'onAdd': tagWrapper});
+  }
+
+  void onAddAll({
+    required TagWrapper Function(int index) builder,
+    required int itemCount,
+  }) {
+    assert(itemCount >= 0);
+    final tagWrappers = List.generate(
+      itemCount,
+      (index) {
+        return builder(index);
+      },
+    );
+    _streamController.sink.add({'onAddAll': tagWrappers});
+  }
+
+  void onInsert(TagWrapper tagWrapper, int index) {
+    assert(index >= 0);
+    _streamController.sink.add({
+      'onInsert': tagWrapper,
+      'index': index,
+    });
+  }
+
+  void onDeleteMulti(List<String> tagNames) {
+    if (tagNames.isEmpty) {
+      return;
+    }
+    _streamController.sink.add({
+      'onDeleteMulti': tagNames,
+    });
+  }
+}
+
+class _AnimationWidget extends StatelessWidget {
+  const _AnimationWidget({
+    required this.animation,
+    required this.child,
+    required this.type,
+  });
+
+  final Animation<double> animation;
+  final Widget child;
+  final TagAnimationType type;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (type) {
+      case TagAnimationType.transition:
+      default:
+        return SizeTransition(
+          sizeFactor: animation,
+          axis: Axis.horizontal,
+          child: child,
+        );
+    }
+  }
+}
+
+class TagWrapper {
+  final String tagName;
+  final RawChip rawChip;
+
+  TagWrapper({required this.tagName, required this.rawChip});
+}
